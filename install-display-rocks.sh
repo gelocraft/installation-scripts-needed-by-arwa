7132135b24414a50bf0876e0db3019d3fb2a3f9f
gelocraft <gelocraft@users.noreply.github.com>
2025-02-12 22:55:04 +0800

    add check guards before cloning

--- install-display-rocks.sh
+++ install-display-rocks.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-git clone https://github.com/gelocraft/async ~/async
-git clone https://github.com/szym/display ~/display
+[ -d ~/async ] || git clone https://github.com/gelocraft/async ~/async
+[ -d ~/display ] || git clone https://github.com/szym/display ~/display
 
 cd ~/async && luarocks make ./async-scm-1.rockspec
 cd ~/display && luarocks make ./display-scm-0.rockspec
